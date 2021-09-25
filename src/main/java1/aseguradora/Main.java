1f2da0e6900a4c989eb6fa474f84a90a79dde512
Fernando Chuquimarca <jf7_chuquimarca@hotmail.com>
2021-09-24 19:51:47 -0500

    aseguradora.

---
+++ src/main/java1/aseguradora/Main.java
@@ -0,0 +1,31 @@
+package aseguradora;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		
+		int op;
+		do {
+		System.out.println("**********Aseguradora Del Sur**********");
+		System.out.println("1.Ingresar Póliza");
+		System.out.println("2.Actualizar Valor");
+		System.out.println("3.Consultar Póliza");
+		System.out.println("4.Imprimir Reporte");
+		System.out.println("5.SALIR");
+		System.out.println("Seleccione su opción: ");
+		op = in.nextInt();
+		switch (op) {
+		
+		
+		}
+		}while (op!= 5);
+		
+		
+	}
+
+}
